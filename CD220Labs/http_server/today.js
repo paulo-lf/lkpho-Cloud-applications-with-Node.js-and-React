91d6635e1e48122dafef3999213236f1e88f6238
Paulo Figueiredo <paulo_linan@me.com>
2026-05-17 11:08:12 +0100

    Challenge Update

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,6 +1,8 @@
  // Export a function named 'getDate' from the module
-module.exports.getDate = function getDate() {
-    // Get the current date and time in the timezone "Australia/Brisbane"
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
-    return aestTime; // Return the formatted date and time
+ module.exports.getDate = function getDate() {
+    // Get the current date and time string in the timezone "Australia/Brisbane"
+    let aestString = new Date().toLocaleString("en-US", { timeZone: "Australia/Brisbane" });
+    // Convert that string back into a Date object
+    let aestDate = new Date(aestString);
+    return aestDate; 
 };
